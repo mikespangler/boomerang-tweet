c3f0a35e1e69dbbb52b2c4d73aca97ff96c6ef1f
Matt Campbell <mecampbell25@gmail.com>
2014-01-19 14:21:08 -0500

    Notes to self

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,3 +1,6 @@
+# This class should hold the code necessary for building a Tweet object
+# Another class - e.g. TwitterApi - should handle the interface with TWTR's API
+
 class Tweet
 
   binding.pry 
